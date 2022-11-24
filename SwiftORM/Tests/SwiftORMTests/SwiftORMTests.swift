c8758d7a1ba9136a3ee8a2a7bd76eb38cea82ee1
ErrorErrorError <erikbautista15@gmail.com>
2022-11-23 21:52:31 -0600

    idk man im tired

---
+++ SwiftORM/Tests/SwiftORMTests/SwiftORMTests.swift
@@ -0,0 +1,11 @@
+import XCTest
+@testable import SwiftORM
+
+final class SwiftORMTests: XCTestCase {
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct
+        // results.
+        XCTAssertEqual(SwiftORM().text, "Hello, World!")
+    }
+}
